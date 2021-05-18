8f6240f16d00226678b865fcddcc4acb7ca15094
kmolski <starfighter413@gmail.com>
2021-05-18 12:32:15 +0200

    Remove `phrase_per_line` option and improve `model_from` signatures

--- node.py
+++ node.py
@@ -1,16 +1,15 @@
-from collections import Counter
 from itertools import chain, islice
 from random import choice, choices
 import re
 
 
-def model_from_str(string, **options):
-    return model_from_lines(string.splitlines(), **options)
+def model_from_str(string, order=2, **format_options):
+    return model_from_lines(string.splitlines(), order=order, **format_options)
 
 
-def model_from_file(file_name, **options):
+def model_from_file(file_name, order=2, **format_options):
     with open(file_name, "r") as input_file:
-        return model_from_lines(input_file, **options)
+        return model_from_lines(input_file, order=order, **format_options)
 
 
 def model_from_lines(lines, order=2, **format_options):
@@ -29,36 +28,30 @@ class Model:
     def add_phrase_from_words(
         self, words, normalize_case=True, remove_non_word_chars=True
     ):
-        def word_processor(s):
-            result = s.strip()
+        def word_processor(word):
+            result = word.strip()
             if normalize_case:
                 result = result.lower()
             if remove_non_word_chars:
                 result = self.__NON_WORD_CHARS.sub("", result)
             return result
 
-        it = (word_processor(w) for w in words)
-        prefix = list(islice(it, self.order - 1))
-        current_word = next(it)
+        words = (word_processor(w) for w in words)
+        prefix = list(islice(words, self.order - 1))
+        current_word = next(words)
 
-        for next_word in it:
+        for next_word in words:
             if current_word not in self.nodes:
                 self.nodes[current_word] = Node(current_word, {})
-            self.nodes[current_word].add_word(tuple(prefix), next_word)
+            self.nodes[current_word].add_word(prefix, next_word)
             prefix.append(current_word)
             prefix.pop(0)
             current_word = next_word
 
-    def add_phrases_from_lines(
-        self, lines, phrase_per_line=False, **word_processor_options
-    ):
-        if phrase_per_line:
-            for line in lines:
-                self.add_phrase_from_words(line.split(), **word_processor_options)
-        else:
-            self.add_phrase_from_words(
-                chain.from_iterable(l.split() for l in lines), **word_processor_options
-            )
+    def add_phrases_from_lines(self, lines, **word_processor_options):
+        self.add_phrase_from_words(
+            chain.from_iterable(l.split() for l in lines), **word_processor_options
+        )
 
     def walk(self, steps, init_word=None):
         if init_word is None:
@@ -71,7 +64,7 @@ class Model:
 
         for _ in range(steps):
             arrows = node.arrows[tuple(init_vector)]
-            word = choices([x for x in arrows.keys()], [x for x in arrows.values()])[0]
+            word = choices(list(arrows.keys()), arrows.values())[0]
             init_vector.append(node.name)
             init_vector.pop(0)
             node = self.nodes[word]
@@ -84,13 +77,12 @@ class Node:
     def __init__(self, name, arrows):
         self.name = name
         self.arrows = arrows
-        self.ref_count = 0
 
     def __repr__(self):
         return self.name
 
     def add_word(self, prefix, word):
-        self.ref_count += 1
+        prefix = tuple(prefix)
         if prefix not in self.arrows:
             self.arrows[prefix] = {}
         if word not in self.arrows[prefix]:
@@ -98,4 +90,9 @@ class Node:
         self.arrows[prefix][word] += 1
 
 
-model = model_from_file("dr_jekyll_and_mr_hyde.txt", order=3)
+jekyll_and_hyde_model = model_from_file(
+    "dr_jekyll_and_mr_hyde.txt",
+    order=3,
+    normalize_case=False,
+    remove_non_word_chars=False,
+)
